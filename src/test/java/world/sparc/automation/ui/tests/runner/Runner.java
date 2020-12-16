ae227d68cc0eb7717507ff1a4cbe9abcc76e675f
Pavel Nagrebetisky <38617666+NahrebetskyiPavel@users.noreply.github.com>
2020-12-16 15:37:12 +0300

    remote

--- src/test/java/world/sparc/automation/ui/tests/runner/Runner.java
+++ src/test/java/world/sparc/automation/ui/tests/runner/Runner.java
@@ -3,19 +3,25 @@ package world.sparc.automation.ui.tests.runner;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import lombok.SneakyThrows;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import io.qameta.allure.selenide.AllureSelenide;
+
+import java.net.URI;
 
 public class Runner {
 
     protected RemoteWebDriver driver;
     protected boolean remote = false;
     ChromeOptions options = new ChromeOptions();
+    DesiredCapabilities capabilities = new DesiredCapabilities();
+
     @BeforeClass
     public static void beforeClass() {
         new DriverSetup().optionsArguments();
@@ -24,9 +30,17 @@ public class Runner {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
     }
 
+    @SneakyThrows
     @Before
     public void setUp() {
-        driver = new ChromeDriver(options);
+        capabilities.setBrowserName("chrome");
+        capabilities.setVersion("86.0");
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", false);
+        capabilities.setCapability("enableLog", true);
+        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+        driver = new RemoteWebDriver(URI.create("http://3.236.124.104:4444/wd/hub").toURL(),capabilities);
+
         com.codeborne.selenide.WebDriverRunner.setWebDriver(driver);
         driver.manage().window().maximize();
     }
